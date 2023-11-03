67ee0a3171be6f8873f7969142f1213cc603867a
Mayuresh1611 <mayur1611personal@gmail.com>
2023-11-03 14:02:15 +0530

    resolved arrangement problems of tasks in list

--- src/main.js
+++ src/main.js
@@ -1,13 +1,79 @@
 var count = 0;
 var tasks = [];
+var editingTask = false
 
 
 
 const taskList = document.querySelector('.task-list')
 const addTask = document.querySelector('.addtask')
 
+// positionoing tasks in the descending order of time
+
+
+function setTaskPosition(task){
+  
+  // requirements
+  // whenever the time is changed
+  // when task relocates iteself it should be in the view
+
+  // if the time is 1 then it should be after 23 
+  // task after the time passed should always be at the lowest
+;
+
+  
+  taskIndex = tasks.indexOf(task)
+  
+  var hours = parseInt(task.childNodes[0].childNodes[0].value , 10)
+  var minutes  = parseInt(task.childNodes[0].childNodes[2].value , 10) 
+  
+  if (isNaN(minutes)) {
+    minutes = 0
+  }
+  var insertPos = count - 1
+  var time = new Date()
+  var currHour = time.getHours() 
+
+  // finding position to insert 
+  for ( var i = count-1 ; i >= 0 ; i--) {
+    if( (parseInt(tasks[i].childNodes[0].childNodes[0].value) > hours  ||
+   /*  && parseInt(task.getAttribute("isPassed")) == 1 && parseInt(tasks[i].getAttribute("isPassed")) == 1) 
+    || (parseInt(tasks[i].childNodes[0].childNodes[0].value) > hours && parseInt(task.getAttribute("isPassed")) == 0 
+    && parseInt(tasks[i].getAttribute("isPassed")) == 0)  || (parseInt(tasks[i].childNodes[0].childNodes[0].value) == hours 
+    && parseInt(tasks[i].childNodes[0].childNodes[2].value) > minutes ) ||*/ (parseInt(tasks[i].childNodes[0].childNodes[0].value) 
+    == hours &&  parseInt(tasks[i].childNodes[0].childNodes[2].value) >= minutes ))) {
+      insertPos = i
+    }
+  }
+  
+  console.log(hours , minutes , insertPos)
+  if (insertPos != count - 1 && taskIndex != insertPos) {
+    console.log("triggered")
+    tasks.splice(taskIndex , 1)
+    tasks.splice(insertPos , 0 , task)
+    
+
+    if (insertPos === 0) {
+        taskList.insertBefore(task , tasks[1])
+    }
+    else{
+      taskList.insertBefore(task , tasks[insertPos + 1])
+    }
+  }
+}
+
+// positions all elements
+function positionAllTasks() {
+  for( var i = 0 ; i < tasks.length ; i++) {
+    setTaskPosition(tasks[i])
+    deselectTask(tasks[i])
+  }
+  
+  
+}
+
 // creates new task element 
 function newTaskElement () {
+  editingTask = true
 
     const val = count
     count += 1                                              // increments the count of the task by 1
@@ -24,7 +90,6 @@ function newTaskElement () {
     // hours field
     const hoursInput = document.createElement('input');
     hoursInput.setAttribute('maxlength', '2');
-
     const colon = document.createTextNode(':');
     // minutes field
     const minutesInput = document.createElement('input');
@@ -35,6 +100,58 @@ function newTaskElement () {
     timeLimit.appendChild(colon);
     timeLimit.appendChild(minutesInput);
 
+    hoursInput.addEventListener('input', function() {
+      // Ensure the input value is within the range 0-23
+      const value = parseInt(this.value, 10);
+      if (isNaN(value) || value < 0) {
+        this.value = '';
+      }
+      else if (value == '' || value == 0){
+        this.value = '00';
+      } 
+      else if (value > 23) {
+        this.value = '';
+      }
+    });
+
+    
+
+    minutesInput.addEventListener('input', function() {
+      // Ensure the input value is within the range 0-60
+      const value = parseInt(this.value, 10);
+      if (isNaN(value) || value < 0) {
+        this.value = '';
+      }
+      else if (value == '' || value == 0){
+        this.value = '00';
+      } 
+      else if (value > 60) {
+        this.value = '';
+      }
+    });
+    var time = new Date()
+    var currHour = time.getHours() 
+    var currMinutes = time.getMinutes()
+
+    // checks and registers if the input time is passed or not
+    minutesInput.addEventListener('blur', function(){
+      if ( hoursInput.value < currHour || (hoursInput.value < currHour &&minutesInput.value < currMinutes)) {
+        hoursInput.parentNode.parentNode.setAttribute("ispassed" , 1)
+      }
+      else{
+        hoursInput.parentNode.parentNode.setAttribute("ispassed" , 0)
+      }
+      console.log("passed?",parseInt(task.getAttribute("isPassed")))
+  
+      positionAllTasks()
+      setTimeout(() => {
+        pos = tasks.indexOf(timeLimit.parentNode)
+        selectTask(tasks[pos])
+      }, 100);
+    });
+
+    
+
     // Create textarea input field
     const taskInput = document.createElement('textarea');
     taskInput.setAttribute('rows', '1');
@@ -43,17 +160,18 @@ function newTaskElement () {
     taskInput.setAttribute('type', 'text');
 
     taskInput.addEventListener('keydown', function(event) {     // removes the next line feature of the enter key 
-      if (event.key === 'Enter' && event.shiftKey === false) { 
+      if (event.key === 'Enter' && event.shiftKey === false  || (event.key === 'Tab')) { 
         taskInput.disabled = true;
+        setTimeout(() => {
+          editingTask = false
+        }, 100);
       }
       else if((event.key === 'Enter') && (event.shiftKey === true)){
+        taskInput.disabled = true;
+        editingTask = false
         addNewTask()
       }
     });
-
-    setTimeout(() => {
-      tasks[val][1] = 1
-    }, 120000);
 
     taskInput.addEventListener( 'input' , () => {             // keeps the height of the textarea dynamic resulting in changing the height of 
       const task = taskInput.closest('.task');                // complete task element
@@ -62,7 +180,6 @@ function newTaskElement () {
       taskInput.rows = Math.max(1 , Math.ceil(rows));
   
       task.style.height = (rows * 20) + 'px'; 
-      console.log(val)
   })
     
     // creates the element for end time of the task (shows the time when the task is completed )
@@ -71,24 +188,33 @@ function newTaskElement () {
     task.appendChild(timeLimit)
     task.appendChild(taskInput)
     task.appendChild(timeEnd)
-
-    return [task , hoursInput,val]
+    return [task , hoursInput ]
 }
 
 // complete functionality When pressed enter on selected task it marks it as complete
 document.addEventListener('keydown', function(event) {
-  if (event.key === 'Enter' && event.shiftKey === false) {
-      if (tasks[pos][1] === 1 && tasks[pos][0].getAttribute('complete') === 'false') {
-          tasks[pos][0].setAttribute('complete', 'true'); // Mark the task as complete
-          console.log(tasks[pos][0].getAttribute('complete'));
-          var timeend = tasks[pos][0].childNodes[2]
-          var time = new Date()
-          timeend.appendChild(
-            document.createTextNode(
-                time.getHours() + ' : '  + time.getMinutes() 
-            )
-          )
-      }
+  if (event.key === 'Enter' && event.shiftKey === false && event.ctrlKey === false && editingTask === false) {
+    // Handle Enter key without Shift (e.g., marking as complete)
+    if (tasks[pos].getAttribute('complete') === 'false') {
+      tasks[pos].setAttribute('complete', 'true'); // Mark the task as complete
+      console.log(tasks[pos].getAttribute('complete'));
+      var timeend = tasks[pos].childNodes[2];
+      var time = new Date();
+      timeend.appendChild(
+        document.createTextNode(
+          time.getHours() + ' : '  + time.getMinutes() 
+        )
+      );
+    }
+  }
+  else if (event.key === 'Enter' && event.ctrlKey === true && editingTask === false) {
+    // Handle Enter key with Ctrl (e.g., marking as incomplete)
+    if (tasks[pos].getAttribute('complete') === 'true') {
+      tasks[pos].setAttribute('complete', 'false'); 
+      // Remove the child node here
+      var timeend = tasks[pos].childNodes[2];
+      timeend.removeChild(timeend.firstChild);
+    }
   }
 });
 
@@ -101,7 +227,7 @@ function addNewTask() {
   taskList.appendChild(ele[0]); // Append the new task element to the taskList
   ele[1].focus()
 
-  tasks.push([ele[0] , 0])
+  tasks.push(ele[0]) 
   
 };
 
@@ -122,9 +248,9 @@ function addNewTask() {
 /* selects the provided task
   it adds the 'selected' name into the class name that way we apply css of selected element */
 function selectTask( task ) {
-  task.childNodes[0].focus()
   task.setAttribute( 'class' , 'task selected')
   task.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  console.log( task.getAttribute("id"))
 }
 
 
@@ -139,8 +265,9 @@ function deselectTask ( task ) {
 var pos = 0 // sets 1st element as default element 
 // handles the selections of the tasks
 document.addEventListener( 'keydown' , function(event) {
+  if (event.key === 'Delete' || event.key === 'ArrowUp' || event.key === 'ArrowDown' ){
   if (event.key === 'Delete') {
-    tasks[pos][0].parentNode.removeChild(tasks[pos][0])
+    tasks[pos].parentNode.removeChild(tasks[pos])
     tasks.splice(pos , 1)
     console.log(pos , count)
 
@@ -149,9 +276,11 @@ document.addEventListener( 'keydown' , function(event) {
     if(pos === tasks.length && pos !== 0){
       pos -= 1;
     }
-  }
 
-  selectTask(tasks[pos][0])
+    if(editingTask == true) {
+      editingTask = false
+    }
+  }
   
   // arrow up
   if (event.key === 'ArrowUp'){
@@ -159,9 +288,9 @@ document.addEventListener( 'keydown' , function(event) {
       
     }
     else{
-      deselectTask(tasks[pos][0])
+      deselectTask(tasks[pos])
       pos -= 1;
-      selectTask(tasks[pos][0])
+      selectTask(tasks[pos])
     }
   }
 
@@ -171,11 +300,12 @@ document.addEventListener( 'keydown' , function(event) {
       
     }
     else{
-      deselectTask(tasks[pos][0])
+      deselectTask(tasks[pos])
       pos += 1;
-      selectTask(tasks[pos][0])
+      selectTask(tasks[pos])
     }
   }
+}
 })
 
 var time = new Date()
